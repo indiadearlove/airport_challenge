e0e0a3419ed881fe8337141a78d8d107f8ca8088
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-14 13:48:37 +0000

    planes can land

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,15 @@
 class Airport
   
+  def initialize
+    @planes =[]
+  end
+
+  def plane_count
+    @planes.count
+  end
+
+  def land(plane)
+    @planes << plane
+  end
+
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,19 @@
+require 'airport.rb'
+require 'plane.rb'
+
 describe Airport do
-  it 'a plane can land' do
-    airport.land(plane)
+
+  let (:airport) {Airport.new}
+  let (:plane) {Plane.new}
+
+  context 'taking off and landing' do
+
+    it 'a plane can land' do
+      expect(airport.plane_count).to eq(0)
+      airport.land(plane)
+      expect(airport.plane_count).to eq(1)
+    end
+
   end
+
 end
